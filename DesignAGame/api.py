a8441bdd288fb0b5d9a5e83f6a908ecfd6f9d84d
Shroomy Shroom <sporeventexplosion@gmail.com>
2016-04-19 17:54:26 +0800

    Part 2

--- DesignAGame/api.py
+++ DesignAGame/api.py
@@ -1,18 +1,57 @@
 import endpoints
 from protorpc import remote, messages
+from google.appengine.api import taskqueue
+from google.appengine.ext import ndb
 
-from models import StringMessage
-from models import User
+from models import StringMessage, GameForm, NewGameForm, ScoreForms, \
+        MakeMoveForm
+from models import User, Game, Score
 
-USER_REQUEST = endpoints.ResourceContainer(username=messages.StringField(1),
-                                           email=messages.StringField(2))
+USER_REQUEST = endpoints.ResourceContainer(
+        username=messages.StringField(1, required=True),
+        email=messages.StringField(2))
+NEW_GAME_REQUEST = endpoints.ResourceContainer(NewGameForm)
+GET_GAME_REQUEST = endpoints.ResourceContainer(
+        urlsafe_game_key=messages.StringField(1))
+MAKE_MOVE_REQUEST = endpoints.ResourceContainer(
+        MakeMoveForm,
+        urlsafe_game_key=messages.StringField(1))
+
+MEMCACHE_TOTAL_MOVES = 'TOTAL_MOVES'
 
 
 @endpoints.api(name='games', version='v1')
 class GamesApi(remote.Service):
+    def _get_user(self, username):
+        user = User.query(User.username==username).get()
+        if not user:
+            raise endpoints.NotFoundException(
+                    'The requested user does not exist!')
+        return user
+
+    def _get_by_urlsafe(self, urlsafe, model):
+        """This function was copied from utils.py in the skeleton project"""
+        try:
+            key = ndb.Key(urlsafe=urlsafe)
+        except TypeError:
+            raise endpoints.BadRequestException('Invalid Key')
+        except Exception, e:
+            if e.__class__.__name__ == 'ProtocolBufferDecodeError':
+                raise endpoints.BadRequestException('Invalid Key')
+            else:
+                raise
+
+        entity = key.get()
+        if not entity:
+            raise endpoints.NotFoundException('Entity cannot be found')
+        if not isinstance(entity, model):
+            raise endpoints.BadRequestException('Incorrect kind')
+        return entity
+
+
     @endpoints.method(request_message=USER_REQUEST,
                       response_message=StringMessage,
-                      path='create_user',
+                      path='user',
                       name='create_user',
                       http_method='POST')
     def create_user(self, request):
@@ -21,6 +60,8 @@ class GamesApi(remote.Service):
         # Without this, the endpoint will fail with a 500, which is not robust
         if len(request.username) > 500:
             raise endpoints.BadRequestException('Username exceeds max length')
+        if request.email is None:
+            raise endpoints.BadRequestException('Email must be specified')
         if len(request.email) > 500:
             raise endpoints.BadRequestException('Email exceeds max length')
 
@@ -31,5 +72,131 @@ class GamesApi(remote.Service):
         # Register the user
         User(username=request.username, email=request.email).put()
         return StringMessage(message='User %s created!' % request.username)
+
+    @endpoints.method(request_message=NEW_GAME_REQUEST,
+                      response_message=GameForm,
+                      path='game',
+                      name='new_game',
+                      http_method='POST')
+    def new_game(self, request):
+        """Start a new game"""
+        user = self._get_user(request.username)
+        try:
+            game = Game.new_game(user.key, request.num_pairs)
+            game.put()
+        except ValueError as ex:
+            raise endpoints.BadRequestException(str(ex))
+
+        # TODO: implement task queues
+        # taskqueue.add(url='/tasks/cache_average_moves')
+        return game.to_form('Good luck playing Concentration!')
+
+    @endpoints.method(request_message=GET_GAME_REQUEST,
+                      response_message=GameForm,
+                      path='game/{urlsafe_game_key}',
+                      name='get_game',
+                      http_method='GET')
+    def get_game(self, request):
+        """Return the current game state."""
+        game = _get_by_urlsafe(request.urlsafe_game_key, Game)
+        if game:
+            return game.to_form('Time to make a move!')
+        else:
+            raise endpoints.NotFoundException('Game not found!')
+
+    @endpoints.method(request_message=MAKE_MOVE_REQUEST,
+                      response_message=GameForm,
+                      path='game/{urlsafe_game_key}',
+                      name='make_move',
+                      http_method='PUT')
+    def make_move(self, request):
+        """Makes a move. Returns a game state with message"""
+        game = self._get_by_urlsafe(request.urlsafe_game_key, Game)
+        card = request.card
+
+        if game.game_over:
+            return game.to_form('Game already over!')
+        if request.card < 0 or card >= len(game.cards):
+            raise endpoints.BadRequestException(
+                    'Index is beyond bounds of the current game')
+        # Check the card has not already been uncovered
+        if (game.cards[card] in game.uncovered_pairs):
+            raise endpoints.BadRequestException(
+                    'The card chosen has already been uncovered')
+        # Check that the card is not the the same as the previous card in a
+        # pair of choices
+        if game.previous_choice != -1 and card == game.previous_choice:
+            raise endpoints.BadRequestException(
+                    'Cannot choose the same card as the first card in a move')
+
+        if game.previous_choice == -1:
+            message = 'You uncover a card'
+            game.is_first_card = True
+            # Get the response first before setting previous_choice avoid
+            # the need of more 'hacks' to get the correct game.previous_choice
+            game.current_choice = card
+            response = game.to_form('message')
+            game.previous_choice = card
+        else:
+            game.current_choice = card
+            game.history.extend([game.previous_choice, card])
+            game.moves += 1
+            game.is_first_card = False
+
+            if game.cards[game.previous_choice] == game.cards[card]:
+                message = 'Matched!'
+                game.uncovered_pairs.append(game.cards[card])
+                if len(game.uncovered_pairs) == len(game.cards) / 2:
+                    game.end_game()
+                    message = 'You win!'
+            else:
+                message = 'Not matched'
+
+            response = game.to_form(message)
+            game.previous_choice = -1
+
+
+        game.put()
+        return response
+
+    @endpoints.method(response_message=ScoreForms,
+                      path='scores',
+                      name='get_scores',
+                      http_method='GET')
+    def get_scores(self, request):
+        """Return all scores"""
+        return ScoreForms(items=[score.to_form() for score in Score.query()])
+
+    @endpoints.method(request_message=USER_REQUEST,
+                      response_message=ScoreForms,
+                      path='scores/user/{username}',
+                      name='get_user_scores',
+                      http_method='GET')
+    def get_user_scores(self, request):
+        """Get all scores of a user"""
+        user = self._get_user(request.username)
+        scores = Score.query(Score.user==user.key)
+        return ScoreForms(items=[score.to_form() for score in scores])
+
+    @endpoints.method(response_message=StringMessage,
+                      path='games/average_attempts',
+                      name='get_average_attempts_remaining',
+                      http_method='GET')
+    def get_average_attempts(self, request):
+        """Get the cached average moves remaining"""
+        return StringMessage(
+                message=memcache.get(MEMCACHE_MOVES_REMAINING) or '')
+
+    @staticmethod
+    def _cache_average_moves():
+        """Populates memcache with the average moves remaining of Games"""
+        games = Game.query(Game.game_over==False, projection=('movew')).fetch()
+        if games:
+            count = len(games)
+            total_moves = sum([game.moves
+                                        for game in games])
+            average = float(total_moves)/count
+            memcache.set(MEMCACHE_TOTAL_MOVES,
+                         'The average moves remaining is %.2f'.format(average))
 
 api = endpoints.api_server([GamesApi])
--- DesignAGame/models.py
+++ DesignAGame/models.py
@@ -3,6 +3,7 @@ from protorpc import messages
 from google.appengine.ext import ndb
 
 import random
+from datetime import datetime
 
 
 class User(ndb.Model):
@@ -16,16 +17,24 @@ class User(ndb.Model):
 
 class Game(ndb.Model):
     """Game object"""
-    pairs = ndb.IntegerProperty(repeated=True, indexed=False)
+    cards = ndb.IntegerProperty(repeated=True, indexed=False)
     uncovered_pairs = ndb.IntegerProperty(repeated=True, indexed=False)
     # the index of number first shown in a pair of numbers to check matches
-    number_shown = ndb.IntegerProperty(indexed=False)
-    attempts = ndb.IntegerProperty(required=True, default=0)
-    game_over = ndb.BooleanProperty(required=True, default=False)
+    previous_choice = ndb.IntegerProperty(indexed=False, default=-1)
+    attempts = ndb.IntegerProperty(default=0)
+    game_over = ndb.BooleanProperty(default=False)
     start_time = ndb.DateTimeProperty(required=True)
-    click_history = ndb.IntegerProperty(repeated=True)
+    end_time = ndb.DateTimeProperty()
+    history = ndb.IntegerProperty(repeated=True, indexed=False)
+    moves = ndb.IntegerProperty(default=0)
     end_time = ndb.DateTimeProperty()
     user = ndb.KeyProperty(required=True, kind='User')
+    # Set by make_move, this stores whether the current card is the first card
+    # in a move, which contains two cards
+    is_first_card = ndb.BooleanProperty(default=True)
+
+    # Only stored temporarily to track current move
+    current_choice = None
 
     @classmethod
     def new_game(cls, user, num_pairs):
@@ -37,12 +46,175 @@ class Game(ndb.Model):
             raise ValueError('Game must have <= 64 pairs of values')
 
         # Make a shuffled list of pairs for the game
-        pairs = range(num_pairs) + range(num_pairs)
-        random.shuffle(pairs)
-        game = Game(user=user, pairs=pairs, attempts=0, game_over=False)
-        game.put()
+        cards = range(num_pairs) * 2
+        random.shuffle(cards)
+        game = Game(user=user, cards=cards, start_time=datetime.now())
+
         return game
 
+    def _uncovered_pairs_to_uncovered_list(self):
+        """
+        For use in GameForm.
+        Generates a list where cards that have been uncovered are shown as
+        their value and cards that have not been uncovered are shown as -1
+        """
+        uncovered = [-1] * len(self.cards)
+
+        for index, card in enumerate(self.cards):
+            if card in self.uncovered_pairs:
+                uncovered[index] = card
+
+        return uncovered
+
+    def _get_matching_card_mapping(self):
+        """
+        Returns a list where the value of one card is the index of the other
+        card with the same value
+        """
+        value_mapping = [[] for i in xrange(len(self.cards) / 2)]
+        for index, value in enumerate(self.cards):
+            value_mapping[value].append(index)
+
+        matching_card_mapping = [None] * len(self.cards)
+
+        for pair in value_mapping:
+            matching_card_mapping[pair[0]] = pair[1]
+            matching_card_mapping[pair[1]] = pair[0]
+
+        return matching_card_mapping
+
+    def _calculate_score(self):
+        """
+        Uses history to generate a score
+        Each successful match is worth 20 points
+        If you fail to match a matching tile which was previously shown,
+        the score is subtracted by 5 times the number of times the tile has
+        been shown.
+        At the end, if there has not been any failed matches, a bonus score of
+        5 times the number of pairs in the level is added.
+        Scoring system from this page: http://dkmgames.com/memory/pairs.php
+        """
+        if not self.game_over:
+            raise ValueError('Cannot caluculate score as game is not over!')
+
+        score = 0
+        perfect_match = True
+        history = self.history
+        cards = self.cards
+        matching_card_mapping = self._get_matching_card_mapping()
+
+        # The number of times a card has been shown, ordered by index.
+        view_count = [0] * len(self.cards)
+
+        paired_history = []
+        for i in xrange(0, len(history), 2):
+            history_pair = (history[i], history[i + 1])
+            paired_history.append(history_pair)
+
+        for move in paired_history:
+            # Check if match
+            if cards[move[0]] == cards[move[1]]:
+                score += 20
+            else:
+                # Get the index of the 'correct' match of the first card
+                correct_match = matching_card_mapping[move[0]]
+
+                # if the correct match has been previously seen by the player
+                if view_count[correct_match] > 0:
+                    score -= view_count[correct_match] * 5
+                    perfect_match = False
+
+                view_count[move[0]] += 1
+                view_count[move[1]] += 1
+
+        if perfect_match:
+            score += len(self.cards) / 2 * 5
+
+        # Make sure the score is not negative
+        return max(score, 0)
+
+    def to_form(self, message):
+        form = GameForm()
+        form.urlsafe_key = self.key.urlsafe()
+        form.username = self.user.get().username
+
+        form.moves = self.moves
+        form.num_pairs = len(self.cards) / 2
+        # Only add the previous choice if this is the second card in a move
+        if not self.is_first_card:
+            form.previous_choice_index = self.previous_choice
+            form.previous_choice_value = self.cards[self.previous_choice]
+        if self.current_choice is not None:
+            form.current_choice_index = self.current_choice
+            form.current_choice_value = self.cards[self.current_choice]
+
+        form.shown_cards = self._uncovered_pairs_to_uncovered_list()
+        form.game_over = self.game_over
+        form.message = message
+        return form
+
+    def end_game(self):
+        self.game_over = True
+        self.end_time = datetime.now()
+        score = self._calculate_score()
+
+        score_entity = Score(user=self.user, datetime=datetime.now(),
+                             score=score, moves=self.moves)
+        score_entity.put()
+
+
+class Score(ndb.Model):
+    """Score object"""
+    user = ndb.KeyProperty(required=True, kind='User')
+    # The time of finishing
+    datetime = ndb.DateTimeProperty(required=True)
+    moves = ndb.IntegerProperty(required=True)
+    score = ndb.IntegerProperty(required=True)
+
+    def to_form(self):
+        return ScoreForm(username=self.user.get().name,
+                         datetime=str(self.datetime), score=self.score,
+                         moves=self.moves)
+
+
+class GameForm(messages.Message):
+    """Game form for outbound data"""
+    urlsafe_key = messages.StringField(1, required=True)
+    username = messages.StringField(2, required=True)
+    moves = messages.IntegerField(3, required=True)
+    num_pairs = messages.IntegerField(4, required=True)
+    # The index and value of the previously chosen card. This is not in the
+    # list of shown cards to avoid confusion
+    previous_choice_index = messages.IntegerField(5)
+    previous_choice_value = messages.IntegerField(6)
+    # The index and value of the current choice
+    current_choice_index = messages.IntegerField(7)
+    current_choice_value = messages.IntegerField(8)
+    # Cards not shown are expressed as -1
+    shown_cards = messages.IntegerField(9, repeated=True)
+    game_over = messages.BooleanField(10, required=True)
+    message = messages.StringField(11, default='')
+
+class NewGameForm(messages.Message):
+    """Create a new game"""
+    username = messages.StringField(1, required=True)
+    num_pairs = messages.IntegerField(2)
+
+class MakeMoveForm(messages.Message):
+    """Used to make a move in an existing game"""
+    card = messages.IntegerField(1, required=True)
+
+class ScoreForm(messages.Message):
+    """ScoreForm for outbound Score information"""
+    username = messages.StringField(1, required=True)
+    datetime = messages.StringField(2, required=True)
+    score = messages.IntegerField(3, required=True)
+    moves = messages.IntegerField(4, required=True)
+
+
+class ScoreForms(messages.Message):
+    """Return multiple ScoreForms"""
+    items = messages.MessageField(ScoreForm, 1, repeated=True)
 
 class StringMessage(messages.Message):
     """
